bb4407a9cc92af92f6a562af643d8a155d6da40c
IMS <gudwnsrh@gmail.com>
2022-06-13 20:48:47 +0900

    22.06.13

---
+++ java_collection/practical_모던자바/part5/Find.java
@@ -0,0 +1,18 @@
+package java_collection.practical_모던자바.part5;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+/*
+ * date: 22.06.13
+ * page: 165
+ */
+
+public class Find {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1,3,5,7,8);
+        Optional<Integer> result = list.stream().parallel().filter(num -> num<4).findAny();
+        System.out.println(result.get());
+    }
+}
