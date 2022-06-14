e089f79a6a1d3feead5bc96a8bb291ef71dd4b05
IMS <gudwnsrh@gmail.com>
2022-06-14 16:44:23 +0900

    22.06.14

--- java_collection/practical_모던자바/part5_stream/Summary.java
+++ java_collection/practical_모던자바/part5_stream/Summary.java
@@ -1,6 +1,7 @@
 package java_collection.practical_모던자바.part5_stream;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Summary {
     public static void main(String[] args) {
@@ -12,7 +13,20 @@ public class Summary {
         map.put("C",3);
         map.forEach((key,value) -> System.out.println("key = " + key + " value = " + value)); 
 
-        Comparator<Integer> comp = (o1, o2) -> Integer.compare(o2,o1);
-        
+        System.out.println("2. stream filter to collection");
+        List<Integer> list = Arrays.asList(1,2,3,4,5,6,7);
+        List<Integer> newList = list.stream().filter(n->n%2==0).collect(Collectors.toCollection(ArrayList::new));
+        System.out.println(newList);
+
+        System.out.println("3. reduce");
+        int sum1 = list.stream().reduce(0, Integer::sum);
+        int sum2 = list.stream().reduce(0, (x,y) -> x+y);
+        int max = list.stream().reduce(0, Integer::max);
+        int min = list.stream().reduce(Integer.MAX_VALUE, Integer::min);
+
+        System.out.println("Integer::sum = " + sum1);
+        System.out.println("(x,y) -> x+y " + sum2);
+        System.out.println("max = " + max);
+        System.out.println("min = " + min);
     }
 }
